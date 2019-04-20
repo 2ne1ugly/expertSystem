6de9e2acc05f69f54ac4094f958cc2249207f276
2ne1ugly <chattermin@gmail.com>
2019-04-20 04:05:45 -0700

    thoght process on how to solve.

--- solve.go
+++ solve.go
@@ -22,6 +22,23 @@ const (
 	Unknown TriBool = 2
 )
 
+/*
+	when statement is solved as true,
+	nodes that are under "AND" or on the root makes next queue
+	nodes under "OR" and "XOR" is used for guessing when both sides are not sure.
+	They all go into special "rule" for checking contradiction.
+	"XOR" is used in guessing anyway.
+
+	when statement is solved as false,
+	the whole proposition is discarded.
+
+	when conclusion is solved,
+	statement is discarded because contrapositive can help the
+	contradiction problem.
+
+	when there's no more queue, they will start to guess.
+*/
+
 //SolveLogics : solves and returns fact table.
 func SolveLogics(input Input) map[byte]TriBool {
 	factTable := make(map[byte]TriBool)
